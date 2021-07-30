e365155bdd6fd4322c3b247aaa82acffd5113fe4
BobHunt <bomazani@icloud.com>
2021-07-29 21:15:32 -0400

    Organized my image files, moved into Assets folder. Removed commented code and white space. Organized HTML to match the order of the NavBar. Organized CSS to closely match the HTML.

--- app.js
+++ app.js
@@ -70,17 +70,30 @@ typewriter = setupTypewriter(typewriter);
 
 typewriter.type();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ***** Shuffle.js *****
 // Basic
 
-var Shuffle = window.Shuffle;
-var element = document.querySelector('.my-shuffle-container');
-var sizer = element.querySelector('.my-sizer-element');
+// var Shuffle = window.Shuffle;
+// var element = document.querySelector('.my-shuffle-container');
+// var sizer = element.querySelector('.my-sizer-element');
 
-var shuffleInstance = new Shuffle(element, {
-  itemSelector: '.picture-item',
-  sizer: sizer // could also be a selector: '.my-sizer-element'
-});
+// var shuffleInstance = new Shuffle(element, {
+//   itemSelector: '.picture-item',
+//   sizer: sizer // could also be a selector: '.my-sizer-element'
+// });
 
 // ****** Advanced with sort buttons *******
 // 'use strict';
